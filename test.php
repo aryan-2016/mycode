f15a8ccefbf7e450a71aa6b0e22b075b8ccfd644
aryan-2016 <ajayjain1511@gmail.com>
2016-03-10 18:03:33 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
 http://stackoverflow.com/questions/14123018/cakephp-2-1-find-using-contain-with-condition
 
 http://stackoverflow.com/questions/26451279/select-records-which-have-atleast-one-hasmany-relation-row-in-cakephp?rq=1
+
+http://ask.amoeba.co.in/joining-multiple-tables-in-cakephp-using-bindmodel-method/
